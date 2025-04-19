b90afe8e2092a9210a47bc885100a8421d083d9f
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-19 13:18:00 +0000

    pnpm -> yarn, macaron -> tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { macaronVitePlugin } from '@macaron-css/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin(), macaronVitePlugin()],
+  plugins: [solidPlugin()],
   server: {
     host: true,
     allowedHosts: ['win-yourself-vite.run.goorm.io'],
