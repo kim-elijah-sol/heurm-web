077e9e3f77fbec1d07f39ff9ed3fad444787d061
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-20 13:43:21 +0000

    widgets(ui): add motivation-card in overview

--- src/widgets/main/ui/overview/overview.ui.tsx
+++ src/widgets/main/ui/overview/overview.ui.tsx
@@ -1,10 +1,11 @@
-import { InProgressCard, OverviewCard } from '~/features/main';
+import { InProgressCard, MotivationCard, OverviewCard } from '~/features/main';
 
 export const Overview = () => {
   return (
     <div>
       <div class='flex gap-3 mb-3'>
         <InProgressCard count={5} />
+        <MotivationCard />
       </div>
       <div class='flex gap-3'>
         <OverviewCard type='win' count={17} />
