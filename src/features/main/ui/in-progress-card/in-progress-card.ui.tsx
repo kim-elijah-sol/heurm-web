43823eb2e8ad4e405a2af59a10de8a367c4c4431
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-20 13:21:05 +0000

    features(ui): in-progress-card

---
+++ src/features/main/ui/in-progress-card/in-progress-card.ui.tsx
@@ -0,0 +1,20 @@
+import type { Component } from 'solid-js';
+
+type Props = {
+  count: number;
+};
+
+export const InProgressCard: Component<Props> = (props) => {
+  const { count } = props;
+
+  const countText = () => count.toLocaleString();
+
+  return (
+    <div class='flex flex-col items-center w-fit border rounded-lg p-3 bg-white border-slate-100 shadow-[0_0_8px_4px_rgba(70,70,70,0.05)]'>
+      <span class='text-sm font-semibold mb-3 text-slate-700'>
+        🚀 In progress
+      </span>
+      <p class='text-4xl font-extrabold text-blue-500'>{countText()}</p>
+    </div>
+  );
+};
--- src/features/main/ui/index.ts
+++ src/features/main/ui/index.ts
@@ -1,1 +1,2 @@
+export * from './in-progress-card';
 export * from './overview-card';
