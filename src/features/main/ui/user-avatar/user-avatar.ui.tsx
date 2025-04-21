02b321be2820763f588cad10ec09c4a23e5a436d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-21 14:32:47 +0000

    features(ui): user-avatar size 40 to 48

--- src/features/main/ui/user-avatar/user-avatar.ui.tsx
+++ src/features/main/ui/user-avatar/user-avatar.ui.tsx
@@ -6,7 +6,7 @@ export const UserAvatar = (props: Props) => {
   const src = () => props.src;
 
   return (
-    <div class='w-10 h-10 rounded-full overflow-hidden border border-gray-300'>
+    <div class='w-12 h-12 rounded-full overflow-hidden border border-gray-300'>
       <img src={src()} alt='' class='w-full h-full' />
     </div>
   );
