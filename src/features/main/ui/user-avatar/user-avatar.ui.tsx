57dd4895d627de3f8da241d81765ab22f840bb3d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-22 10:22:13 +0000

    widgets/main(ui): apply blured-panel

--- src/features/main/ui/user-avatar/user-avatar.ui.tsx
+++ src/features/main/ui/user-avatar/user-avatar.ui.tsx
@@ -1,12 +1,18 @@
+import type { JSX } from 'solid-js';
+
 type Props = {
   src: string;
+  onClick?: JSX.CustomEventHandlersNamespaced<HTMLDivElement>['on:click'];
 };
 
 export const UserAvatar = (props: Props) => {
   const src = () => props.src;
 
   return (
-    <div class='w-12 h-12 rounded-full overflow-hidden border border-gray-300'>
+    <div
+      on:click={props.onClick}
+      class='w-12 h-12 rounded-full overflow-hidden border border-gray-300'
+    >
       <img src={src()} alt='' class='w-full h-full' />
     </div>
   );
--- src/widgets/main/ui/main-top/main-top.ui.tsx
+++ src/widgets/main/ui/main-top/main-top.ui.tsx
@@ -1,10 +1,20 @@
+import { createSignal } from 'solid-js';
 import { HelloUser, UserAvatar } from '~/features/main';
+import { BluredPanel } from '../blured-panel';
 
 export const MainTop = () => {
+  const [isBluredPanelShow, setIsBluredPanelShow] = createSignal(false);
+
   return (
     <div class='flex items-center justify-between mb-4'>
       <HelloUser userName='Sol' />
-      <UserAvatar src='https://avatars.githubusercontent.com/u/86874556?v=4' />
+      <UserAvatar
+        onClick={() => setIsBluredPanelShow(true)}
+        src='https://avatars.githubusercontent.com/u/86874556?v=4'
+      />
+      {isBluredPanelShow() && (
+        <BluredPanel close={() => setIsBluredPanelShow(false)} />
+      )}
     </div>
   );
 };
