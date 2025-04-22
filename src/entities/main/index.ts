acddbf13826e5a7275a8afcf5cf7570204507aea
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-22 13:58:01 +0000

    entities(hook): add useDateSelect hook

--- src/entities/main/index.ts
+++ src/entities/main/index.ts
@@ -1,2 +1,3 @@
 export * from './hello-writing.constant';
 export * from './motivation-writing.constant';
+export * from './useDateSelect.hook';
---
+++ src/entities/main/useDateSelect.hook.ts
@@ -0,0 +1,31 @@
+import { createSignal } from 'solid-js';
+
+export const useDateSelect = () => {
+  const start = new Date('2025-03-21');
+
+  const end = new Date('2025-04-22');
+
+  const [current, setCurrent] = createSignal(new Date('2025-04-22'));
+
+  const dates = () => {
+    const dates = [];
+    let currentDate = new Date(start);
+
+    if (end < start) {
+      return [];
+    }
+
+    while (currentDate <= end) {
+      dates.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dates;
+  };
+
+  return {
+    current,
+    setCurrent,
+    dates,
+  };
+};
