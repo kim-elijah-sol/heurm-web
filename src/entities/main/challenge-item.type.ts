ff25ec3f92346f6a4dac97f808789fa283526cbb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-23 13:35:05 +0000

    entities(type): develop challenge item type

--- src/entities/main/challenge-item.type.ts
+++ src/entities/main/challenge-item.type.ts
@@ -1,19 +1,30 @@
+type ChallengeItemBase = {
+  name: string;
+};
+
 type CountableType = {
   targetCount: number;
   count: number | null;
 };
 
-export type ChallengeItemType = {
-  name: string;
-} & (
-  | {
-      type: 'complete';
-      isCompleted: boolean | null;
-    }
-  | ({
-      type: 'over';
-    } & CountableType)
-  | ({
-      type: 'under';
-    } & CountableType)
-);
+type CompletableType = {
+  type: 'complete';
+  isCompleted: boolean | null;
+};
+
+export type CompleteChallengeItemType = ChallengeItemBase & CompletableType;
+
+export type OverChallengeItemType = ChallengeItemBase &
+  CountableType & {
+    type: 'over';
+  };
+
+export type UnderChallengeItemType = ChallengeItemBase &
+  CountableType & {
+    type: 'under';
+  };
+
+export type ChallengeItemType =
+  | CompleteChallengeItemType
+  | OverChallengeItemType
+  | UnderChallengeItemType;
