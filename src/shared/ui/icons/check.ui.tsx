8d221ea5e1b8aba19e149e3e0b76ab27939b1d6e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-25 10:39:23 +0000

    shared(ui): add icons

---
+++ src/shared/ui/icons/check.ui.tsx
@@ -0,0 +1,16 @@
+export const Check = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='36'
+    height='36'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='#FFFFFF'
+    stroke-width='2'
+    stroke-linecap='round'
+    stroke-linejoin='round'
+    class='lucide lucide-check-icon lucide-check'
+  >
+    <path d='M20 6 9 17l-5-5' />
+  </svg>
+);
---
+++ src/shared/ui/icons/index.ts
@@ -0,0 +1,3 @@
+export * from './ban.ui';
+export * from './check.ui';
+export * from './loader.ui';
