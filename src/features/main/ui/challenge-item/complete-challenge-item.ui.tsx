c9236f3bc47afa2940097d744c7c24cba68c60f7
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-25 10:39:52 +0000

    features(ui): apply icons in `complete-challenge-item`

--- src/features/main/ui/challenge-item/complete-challenge-item.ui.tsx
+++ src/features/main/ui/challenge-item/complete-challenge-item.ui.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { createSignal, splitProps } from 'solid-js';
 import { CompleteChallengeItemType } from '~/entities/main';
+import { Ban, Check, Loader } from '~/shared/ui';
 import { BluredPanel } from '../blured-panel';
 
 type Props = CompleteChallengeItemType & {
@@ -44,21 +45,7 @@ export const Complete = (originProps: Props) => {
                   )}
                   on:click={() => rest.onChange(false)}
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='36'
-                    height='36'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='#FFFFFF'
-                    stroke-width='2'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    class='lucide lucide-ban-icon lucide-ban'
-                  >
-                    <circle cx='12' cy='12' r='10' />
-                    <path d='m4.9 4.9 14.2 14.2' />
-                  </svg>
+                  <Ban />
                 </button>
                 <button
                   class={clsx(
@@ -67,20 +54,7 @@ export const Complete = (originProps: Props) => {
                   )}
                   on:click={() => rest.onChange(true)}
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='36'
-                    height='36'
-                    viewBox='0 0 24 24'
-                    fill='none'
-                    stroke='#FFFFFF'
-                    stroke-width='2'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    class='lucide lucide-check-icon lucide-check'
-                  >
-                    <path d='M20 6 9 17l-5-5' />
-                  </svg>
+                  <Check />
                 </button>
               </div>
               <button
@@ -90,27 +64,7 @@ export const Complete = (originProps: Props) => {
                 )}
                 on:click={() => rest.onChange(null)}
               >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='36'
-                  height='36'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='#FFFFFF'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  class='lucide lucide-loader-icon lucide-loader'
-                >
-                  <path d='M12 2v4' />
-                  <path d='m16.2 7.8 2.9-2.9' />
-                  <path d='M18 12h4' />
-                  <path d='m16.2 16.2 2.9 2.9' />
-                  <path d='M12 18v4' />
-                  <path d='m4.9 19.1 2.9-2.9' />
-                  <path d='M2 12h4' />
-                  <path d='m4.9 4.9 2.9 2.9' />
-                </svg>
+                <Loader />
               </button>
             </div>
           )}
