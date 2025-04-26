813567b2eff9d7d9156e57a8b75768e552b73470
kim-elijah-sol <kimelijahsol@gmail.com>
2025-04-26 13:18:12 +0000

    features(ui): user-panel in `user-avatar`

--- src/widgets/main/ui/main-top/main-top.ui.tsx
+++ src/widgets/main/ui/main-top/main-top.ui.tsx
@@ -1,21 +1,10 @@
-import { createSignal } from 'solid-js';
-import { BluredPanel, HelloUser, UserAvatar } from '~/features/main';
+import { HelloUser, UserAvatar } from '~/features/main';
 
 export const MainTop = () => {
-  const [isBluredPanelShow, setIsBluredPanelShow] = createSignal(false);
-
   return (
     <div class='flex items-center justify-between mb-4'>
       <HelloUser userName='Sol' />
-      <UserAvatar
-        onClick={() => setIsBluredPanelShow(true)}
-        src='https://avatars.githubusercontent.com/u/86874556?v=4'
-      />
-      {isBluredPanelShow() && (
-        <BluredPanel close={() => setIsBluredPanelShow(false)}>
-          {() => <div class='w-full h-full touch-none'></div>}
-        </BluredPanel>
-      )}
+      <UserAvatar src='https://avatars.githubusercontent.com/u/86874556?v=4' />
     </div>
   );
 };
