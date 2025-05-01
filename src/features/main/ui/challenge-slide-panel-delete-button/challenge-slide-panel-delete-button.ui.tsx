803596554e61844c51c3dd3d00cec045779648db
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-01 12:57:59 +0000

    features(ui): change active bg color `challenge-slide-panel-delete-button`

--- src/features/main/ui/challenge-slide-panel-delete-button/challenge-slide-panel-delete-button.ui.tsx
+++ src/features/main/ui/challenge-slide-panel-delete-button/challenge-slide-panel-delete-button.ui.tsx
@@ -1,6 +1,6 @@
 export const ChallengeSlidePanelDeleteButton = () => {
   return (
-    <button class='font-semibold text-slate-300 py-2 px-3 rounded-[12px] active:bg-slate-200 active:scale-95 transition-all'>
+    <button class='font-semibold text-slate-300 py-2 px-3 rounded-[12px] active:bg-slate-100 active:scale-95 transition-all'>
       Delete This Challenge
     </button>
   );
