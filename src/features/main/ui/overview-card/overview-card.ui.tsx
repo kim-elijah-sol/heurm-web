1fdf0b2940d7a3b455d6d007d5212bcd697ac0ea
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-02 14:54:22 +0000

    features(ui): add flex styling in `overview-card`

--- src/features/main/ui/overview-card/overview-card.ui.tsx
+++ src/features/main/ui/overview-card/overview-card.ui.tsx
@@ -18,7 +18,13 @@ export const OverviewCard: Component<Props> = (props) => {
     );
 
   return (
-    <div class='flex flex-1 flex-col items-center rounded-lg p-3 border bg-white border-slate-100 shadow-[0_0_8px_4px_rgba(70,70,70,0.05)]'>
+    <div
+      style={{
+        flex: props.count || 1,
+        'min-width': '100px',
+      }}
+      class='flex flex-col items-center rounded-lg p-3 transition-all border bg-white border-slate-100 shadow-[0_0_8px_4px_rgba(70,70,70,0.05)]'
+    >
       <span class='text-sm font-semibold mb-3 text-slate-700'>{title()}</span>
       <p class={countClass()}>{countText()}</p>
     </div>
