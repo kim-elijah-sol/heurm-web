d5a6e4c90ab702e01b15870c9cb5094bfe6f927e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-12 10:36:28 +0000

    features(join/ui): `join-banner`

---
+++ src/features/join/ui/join-banner/join-banner.ui.tsx
@@ -0,0 +1,13 @@
+import './join-banner.ui.css';
+
+export const JoinBanner = () => {
+  return (
+    <div class='join-banner'>
+      <p class='text-center font-bold text-3xl leading-12 relative overflow-hidden mb-10 login-banner'>
+        Join is
+        <br />
+        First Win!
+      </p>
+    </div>
+  );
+};
