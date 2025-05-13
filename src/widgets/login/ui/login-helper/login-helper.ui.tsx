b45e79f3381349166cff33843ff1b8444a3e5ffe
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-13 13:36:09 +0000

    widgets(reset-password/ui): `reset-password-panel` base setting

--- src/widgets/login/ui/login-helper/login-helper.ui.tsx
+++ src/widgets/login/ui/login-helper/login-helper.ui.tsx
@@ -2,9 +2,16 @@ import clsx from 'clsx';
 import { createBoolean } from '~/shared/hook';
 import { KeyRound, UserPlus } from '~/shared/ui';
 import { JoinPanel } from '~/widgets/join/ui';
+import { ResetPasswordPanel } from '~/widgets/reset-password/ui';
 
 export const LoginHelper = () => {
   const [isJoinPanel, openJoinPanel, closeJoinPanel] = createBoolean();
+
+  const [
+    isResetPasswordPanel,
+    openResetPasswordPanel,
+    closeResetPasswordPanel,
+  ] = createBoolean();
 
   const buttonBaseClassName =
     'p-4 rounded-[35%] transition-all active:scale-95';
@@ -13,6 +20,7 @@ export const LoginHelper = () => {
     <div class='w-full flex justify-center gap-6'>
       <button
         class={clsx(buttonBaseClassName, 'bg-rose-400 active:bg-rose-500')}
+        onClick={openResetPasswordPanel}
       >
         <KeyRound />
       </button>
@@ -24,6 +32,9 @@ export const LoginHelper = () => {
       </button>
 
       {isJoinPanel() && <JoinPanel close={closeJoinPanel} />}
+      {isResetPasswordPanel() && (
+        <ResetPasswordPanel close={closeResetPasswordPanel} />
+      )}
     </div>
   );
 };
---
+++ src/widgets/reset-password/ui/reset-password-panel/reset-password-panel.ui.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'solid-js';
+import { BluredPanel, X } from '~/shared/ui';
+
+type Props = {
+  close: () => void;
+};
+
+export const ResetPasswordPanel: Component<Props> = (props) => {
+  return (
+    <BluredPanel close={props.close} autoClose={false}>
+      {(close) => (
+        <div class='w-full h-full touch-none flex flex-col items-center justify-center px-4'>
+          <button
+            type='button'
+            onClick={close}
+            class='p-2 rounded-[35%] transition-all active:scale-90 active:bg-red-600 bg-red-500 absolute right-6 top-6'
+          >
+            <X />
+          </button>
+        </div>
+      )}
+    </BluredPanel>
+  );
+};
