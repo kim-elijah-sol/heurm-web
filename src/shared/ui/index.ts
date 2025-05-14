ec2db21e640754a15d8d2fd51eee76b3f98df494
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-14 13:21:21 +0000

    shared(ui): remove unused ui kit

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,4 @@
 export * from './blured-panel';
-export * from './button';
 export * from './challenge-color-select';
 export * from './icons';
-export * from './input';
 export * from './slide-panel';
