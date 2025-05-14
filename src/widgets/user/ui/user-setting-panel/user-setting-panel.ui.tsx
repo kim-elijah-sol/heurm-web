c99d83b70f23e8992f256533608718b8256f7ee5
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-14 14:11:19 +0000

    widgets(user/ui): `user-setting-panel`

--- src/widgets/user/ui/user-setting-panel/user-setting-panel.ui.tsx
+++ src/widgets/user/ui/user-setting-panel/user-setting-panel.ui.tsx
@@ -9,20 +9,39 @@ export const UserSettingPanel: Component<Props> = (props) => {
   return (
     <Panel.Slide close={props.close}>
       {(close) => (
-        <div class='flex-1 flex flex-col gap-4'>
-          <button
-            onClick={close}
-            class='p-[10px] rounded-[35%] transition-all bg-red-400 active:bg-red-500 active:scale-95 self-end'
-          >
-            <X size={24} />
-          </button>
+        <div class='flex-1 pb-20'>
+          <div class='w-full min-h-full flex flex-col justify-between'>
+            <div class='flex flex-col gap-4'>
+              <button
+                onClick={close}
+                class='p-[10px] rounded-[35%] transition-all bg-red-400 active:bg-red-500 active:scale-95 self-end'
+              >
+                <X size={24} />
+              </button>
 
-          <div class='w-full'>
-            <p class='mb-3 ml-1 font-semibold text-gray-500'>Your Name</p>
-            <input
-              type='text'
-              class='font-semibold text-xl py-2 px-3 rounded-[12px] bg-slate-100 transition-all duration-300 w-full focus:bg-slate-200'
-            />
+              <div class='w-full'>
+                <p class='mb-3 ml-1 font-semibold text-gray-500'>Your Name</p>
+                <input
+                  type='text'
+                  class='font-semibold text-xl py-2 px-3 rounded-[12px] bg-slate-100 transition-all duration-300 w-full focus:bg-slate-200'
+                />
+              </div>
+
+              <div class='w-full'>
+                <p class='mb-3 ml-1 font-semibold text-gray-500'>
+                  New Password
+                </p>
+                <input
+                  type='password'
+                  maxlength={16}
+                  class='font-semibold text-xl py-2 px-3 rounded-[12px] bg-slate-100 transition-all duration-300 w-full focus:bg-slate-200'
+                />
+              </div>
+            </div>
+
+            <button class='font-semibold text-slate-300 py-2 px-3 rounded-[12px] active:bg-slate-100 active:scale-95 transition-all'>
+              Cancel Account
+            </button>
           </div>
 
           <Panel.CTAButton color={() => 'green'}>Save</Panel.CTAButton>
