4f9284e79cc6979b4e8c2b144e31c0fd765dd460
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-16 14:29:14 +0000

    widgets(main/ui): add contact at `footer`

--- src/widgets/main/ui/footer/footer.ui.tsx
+++ src/widgets/main/ui/footer/footer.ui.tsx
@@ -2,6 +2,41 @@ export const Footer = () => {
   return (
     <footer class='px-4 py-6'>
       <div class='w-full h-[1px] bg-linear-to-r from-white via-slate-300 to-white mb-6' />
+
+      <div class='flex flex-col gap-2'>
+        <p class='font-semibold text-slate-400 text-sm text-center'>
+          Win Yourself by
+          <a
+            href='mailto:kimelijahsol@gmail.com'
+            class='font-extrabold ml-2 text-slate-600'
+          >
+            @kim-elijah-sol
+          </a>
+        </p>
+        <p class='font-semibold text-slate-400 text-sm text-center'>
+          feedback & support
+          <a
+            href='mailto:cx@winyourself.com'
+            class='font-extrabold ml-2 text-slate-600'
+          >
+            cx@winyourself.com
+          </a>
+        </p>
+        <p class='font-semibold text-slate-400 text-sm text-center'>
+          github
+          <a
+            href='https://github.com/kim-elijah-sol'
+            target='_blank'
+            class='font-extrabold ml-2 text-slate-600'
+          >
+            @kim-elijah-sol
+          </a>
+        </p>
+      </div>
+
+      <p class='mt-6 font-semibold text-slate-400 text-sm text-center'>
+        © 2025 Win Yourself. All rights reserved.
+      </p>
     </footer>
   );
 };
