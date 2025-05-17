4185d34b49f45877b180a3350c9e9140e0fcf68e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-17 13:55:34 +0000

    shared(ui): `toast-item` re-size

--- src/shared/ui/toast-item/toast-item.ui.tsx
+++ src/shared/ui/toast-item/toast-item.ui.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const ToastItem: Component<Props> = (props) => {
   return (
     <div
-      class='toast fixed top-8 bg-gray-500/70 flex items-center justify-center backdrop-blur-md rounded-[24px] py-1 px-4 left-4 right-4 text-white font-semibold min-h-12 z-[99]'
+      class='toast fixed top-8 bg-gray-500/70 flex items-center justify-center text-center text-sm backdrop-blur-md rounded-[24px] py-2 px-6 left-4 right-4 text-white font-semibold min-h-12 z-[99]'
       innerHTML={props.text}
     />
   );
