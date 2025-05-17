1111711df4d008c230a65836c0ae6b611d81fdf0
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-17 13:35:17 +0000

    app: `toast-portal`

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,17 @@ import { render } from 'solid-js/web';
 import Login from '~/pages/login';
 import Main from '~/pages/main';
 import './style/reset.css';
+import { ToastPortal } from './toast-portal';
 
 render(
   () => (
-    <Router>
-      <Route path='/' component={Main} />
-      <Route path='/login' component={Login} />
-    </Router>
+    <>
+      <Router>
+        <Route path='/' component={Main} />
+        <Route path='/login' component={Login} />
+      </Router>
+      <ToastPortal />
+    </>
   ),
   document.getElementById('root')!
 );
---
+++ src/app/toast-portal/toast-portal.ui.tsx
@@ -0,0 +1,42 @@
+import { createSignal, onCleanup, onMount } from 'solid-js';
+import { For, Portal } from 'solid-js/web';
+import { ADD_TOAST_EVENT_NAME } from '~/shared/constant';
+import { Toast } from '~/shared/model';
+import { ToastItem } from '~/shared/ui';
+
+export const ToastPortal = () => {
+  const [toasts, setToasts] = createSignal<Toast[]>([]);
+
+  const handleAddToast = (event: Event) => {
+    const {
+      detail: { text },
+    } = event as CustomEvent<{ text: string }>;
+
+    const toast: Toast = {
+      id: new Date().valueOf(),
+      text,
+    };
+
+    if (toasts().some((toast) => toast.text === text)) return;
+
+    setToasts((toasts) => toasts.concat(toast));
+
+    setTimeout(() => {
+      setToasts((toasts) => toasts.filter(({ id }) => id !== toast.id));
+    }, 3000);
+  };
+
+  onMount(() => {
+    window.addEventListener(ADD_TOAST_EVENT_NAME, handleAddToast);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener(ADD_TOAST_EVENT_NAME, handleAddToast);
+  });
+
+  return (
+    <Portal mount={document.querySelector('#toast-portal') as Node}>
+      <For each={toasts()}>{(toast) => <ToastItem text={toast.text} />}</For>
+    </Portal>
+  );
+};
