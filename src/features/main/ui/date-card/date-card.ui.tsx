4dfa189e8159334e955586694cf52f8df221a86e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-18 13:16:19 +0000

    features(main/ui): current date design at `date-card`

--- src/features/main/ui/date-card/date-card.ui.tsx
+++ src/features/main/ui/date-card/date-card.ui.tsx
@@ -15,13 +15,20 @@ export const DateCard: Component<Props> = (props) => {
   return (
     <div
       class={clsx(
-        'date-card relative rounded-[35%] text-white font-semibold flex items-center justify-center transition-all duration-300 active:scale-90',
+        'date-card relative rounded-[35%] text-white text-lg font-semibold flex items-center justify-center transition-all duration-300 active:scale-90',
         props.status() === 'win'
-          ? 'bg-emerald-300 active:bg-emerald-400'
+          ? props.isCurrent()
+            ? 'bg-emerald-400 active:bg-emerald-500'
+            : 'bg-emerald-300 active:bg-emerald-400'
           : props.status() === 'lose'
-          ? 'bg-rose-300 active:bg-rose-400'
+          ? props.isCurrent()
+            ? 'bg-rose-400 active:bg-rose-500'
+            : 'bg-rose-300 active:bg-rose-400'
+          : props.isCurrent()
+          ? 'bg-gray-400 active:bg-gray-500'
           : 'bg-gray-300 active:bg-gray-400'
       )}
+      onClick={props.onClick}
     >
       {props.date().getDate()}
 
