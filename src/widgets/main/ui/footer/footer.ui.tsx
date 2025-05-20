0e454ae11775cf9b06d5e65928344178e0089a2d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-20 13:44:08 +0000

    widgets(main/ui): add bg-slate-100/75 at `footer`

--- src/widgets/main/ui/footer/footer.ui.tsx
+++ src/widgets/main/ui/footer/footer.ui.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer class='px-4 py-6'>
+    <footer class='px-4 pb-6 mt-6 bg-slate-100/75'>
       <div class='w-full h-[1px] bg-linear-to-r from-white via-slate-300 to-white mb-6' />
 
       <div class='flex flex-col gap-2'>
