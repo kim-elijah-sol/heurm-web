b933946761e71f92705eaed379e040088fe7a8fd
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-31 14:06:40 +0000

    shared(constant): `storage-keys`

--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -2,3 +2,4 @@ export * from './add-toast-event-name.constant';
 export * from './challenge-color.constant';
 export * from './challenge-day.constant';
 export * from './default-error-message.constant';
+export * from './storage-keys.constant';
---
+++ src/shared/constant/storage-keys.constant.ts
@@ -0,0 +1,5 @@
+export const STORAGE_KEYS = {
+  ACCESS_TOKEN: '--wys-at',
+  REFRESH_TOKEN: '--wys-rt',
+  CLIENT_ID: '--wys-ci',
+} as const;
