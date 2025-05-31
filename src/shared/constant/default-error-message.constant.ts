9a16edadc6a1de9bc0693f2a40978b28940bd7d2
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-31 13:54:02 +0000

    shared(constant): `default-error-message`

---
+++ src/shared/constant/default-error-message.constant.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_ERROR_MESSAGE =
+  'Oops! An unexpected error occurred.<br/>Please retry or contact us';
--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -1,3 +1,4 @@
 export * from './add-toast-event-name.constant';
 export * from './challenge-color.constant';
 export * from './challenge-day.constant';
+export * from './default-error-message.constant';
