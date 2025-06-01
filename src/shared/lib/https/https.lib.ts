9d4116abd151f7de575873ebb757d5cdb18f1370
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-01 10:38:23 +0000

    shared(lib): refresh access token at `https`

--- src/shared/lib/https/https.lib.ts
+++ src/shared/lib/https/https.lib.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, isAxiosError } from 'axios';
 import { STORAGE_KEYS } from '~/shared/constant';
 
 export const https = axios.create({
@@ -13,4 +13,35 @@ https.interceptors.request.use((config) => {
   }
 
   return config;
+});
+
+https.interceptors.response.use(undefined, async (error) => {
+  if (
+    isAxiosError(error) &&
+    error.response?.status === 401 &&
+    error.response?.data === 'Access token is incorrect'
+  ) {
+    const refreshResult = await https.post('/user/refresh', {
+      refreshToken: localStorage.getItem(STORAGE_KEYS.REFRESH_TOKEN),
+      clientId: localStorage.getItem(STORAGE_KEYS.CLIENT_ID),
+    });
+
+    const accessToken = refreshResult.data.accessToken;
+
+    if (accessToken) {
+      const originalRequest = error.config as AxiosRequestConfig;
+
+      localStorage.setItem(STORAGE_KEYS.ACCESS_TOKEN, accessToken);
+
+      originalRequest.headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      };
+
+      const response = await https(originalRequest);
+      return response;
+    }
+  }
+
+  return Promise.reject(error);
 });
