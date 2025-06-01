d1f247edfff622a98f0c1ed8f5532b67ab2fc093
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-01 10:13:59 +0000

    shared(lib): https access token sending request interceptor

--- src/shared/lib/https/https.lib.ts
+++ src/shared/lib/https/https.lib.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
+import { STORAGE_KEYS } from '~/shared/constant';
 
 export const https = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+https.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN);
+
+  if (accessToken !== null) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
