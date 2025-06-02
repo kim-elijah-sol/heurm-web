c6fd9cfb5e90bdafdb2e92849bc681c815df0a69
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-02 12:55:13 +0000

    app: page component lazy loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { Route, Router } from '@solidjs/router';
+import { lazy } from 'solid-js';
 import { render } from 'solid-js/web';
-import Login from '~/pages/login';
-import Main from '~/pages/main';
 import { QueryClientProvider } from './query-client-provider';
 import './style/reset.css';
 import { ToastPortal } from './toast-portal';
+
+const Login = lazy(() => import('~/pages/login'));
+
+const Main = lazy(() => import('~/pages/main'));
 
 render(
   () => (
