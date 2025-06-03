ae8ec1c6a1b598eb772f7184d930c3aaeb314947
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-03 13:54:03 +0000

    shared(ui): `server-image`

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
 export * from './challenge-color-select';
 export * from './icons';
 export * from './panel';
+export * from './server-image';
 export * from './toast-item';
---
+++ src/shared/ui/server-image/server-image.ui.tsx
@@ -0,0 +1,13 @@
+import { Component, JSX, splitProps } from 'solid-js';
+
+type ImgProps = JSX.IntrinsicElements['img'];
+
+type Props = Required<Pick<ImgProps, 'src'>> & Omit<ImgProps, 'src'>;
+
+export const ServerImage: Component<Props> = (props) => {
+  const [localProps, rest] = splitProps(props, ['src']);
+
+  const src = () => localProps.src;
+
+  return <img src={`${import.meta.env.VITE_API_BASE_URL}${src()}`} {...rest} />;
+};
