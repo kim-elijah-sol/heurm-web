383da0beb830e6e9069a74a7300291467196d6ed
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-20 13:57:08 +0000

    feature: 챌린지 아이템 생성 패널 재개발 > 이름, 타입 선택 폼

--- src/widgets/challenge-edit/challenge-edit-panel.ui.tsx
+++ src/widgets/challenge-edit/challenge-edit-panel.ui.tsx
@@ -407,6 +407,7 @@ export const ChallengeEditPanel: Component<Props> = (props) => {
                     handleNewChallengeItem(it);
                   }}
                   close={newChallengeItemClose}
+                  color={color}
                 />
               )}
             </div>
--- src/widgets/new-challenge-item/new-challenge-item-panel.ui.tsx
+++ src/widgets/new-challenge-item/new-challenge-item-panel.ui.tsx
@@ -1,126 +1,161 @@
-import { type Accessor, type Component } from 'solid-js';
-import { ChallengeEditType } from '~/entities/challenge-edit';
-import { createNewChallengeItemForm } from '~/features/new-challenge-item/hook';
+import clsx from 'clsx';
 import {
-  NewChallengeItemStep,
-  NewChallengeItemStepNavigator,
-} from '~/features/new-challenge-item/ui';
-import { toast } from '~/shared/lib';
-import { Panel } from '~/shared/ui';
-import './new-challenge-item-step.ui.css';
+  children,
+  createSignal,
+  type Accessor,
+  type Component,
+  type JSX,
+} from 'solid-js';
+import { ChallengeEditType } from '~/entities/challenge-edit';
+import { CHALLENGE_TEXT_COLOR_500 } from '~/shared/constant';
+import type { ChallengeColor, ChallengeItemType } from '~/shared/types';
+import { CheckCheck, ChevronsDown, ChevronsUp, Panel, X } from '~/shared/ui';
 
 type Props = {
   onSubmit: (challengeItem: ChallengeEditType.ChallengeItemForm) => void;
   close: () => void;
+  color: Accessor<ChallengeColor>;
 };
 
 export const NewChallengeItemPanel: Component<Props> = (props) => {
-  const {
-    setStep,
-    type,
-    setType,
-    name,
-    setName,
-    count,
-    setCount,
-    unit,
-    setUnit,
-    day,
-    handleChangeDay,
-    getDisplayType,
-    maxStep,
-    currentStep,
-  } = createNewChallengeItemForm();
+  const inputBaseClassName =
+    'font-semibold px-4 py-4 rounded-[24px] w-full transition-all bg-slate-100 focus:bg-slate-200';
+
+  const [name, setName] = createSignal<string>('');
+
+  const [type, setType] =
+    createSignal<Uppercase<ChallengeItemType>>('COMPLETE');
+
+  const typeIndicatorLevel = () =>
+    type() === 'COMPLETE' ? 0 : type() === 'OVER' ? 1 : 2;
 
   return (
-    <Panel.Blured close={props.close} autoClose={false}>
+    <Panel.Slide close={props.close}>
       {(close) => (
-        <div class='w-full h-full relative'>
-          <Panel.CloseButton onClick={close} />
+        <>
+          <div class='absolute flex items-center justify-between left-0 right-0 top-0 p-4 pb-2 bg-white/75 backdrop-blur-sm'>
+            <p class='font-semibold text-2xl'>
+              {name().trim().length > 0 ? name().trim() : 'New Challenge Item'}
+            </p>
+            <button
+              onClick={close}
+              class='p-[7px] rounded-[42%] transition-all active:scale-[.95] bg-red-400 active:bg-red-500'
+            >
+              <X size={30} />
+            </button>
+          </div>
+          <div class='flex-1 overflow-y-auto flex flex-col items-center pb-20 pt-[72px]'>
+            <Form.Wrapper>
+              <Form.Label>Name</Form.Label>
+              <input
+                type='text'
+                class={inputBaseClassName}
+                value={name()}
+                onInput={(e) => setName(e.target.value)}
+              />
+            </Form.Wrapper>
+            <Form.Wrapper>
+              <Form.Label>Type</Form.Label>
+              <div class='flex relative w-full h-14 bg-slate-100 rounded-[24px] p-[6px] gap-2'>
+                <label
+                  for='complete'
+                  class={clsx(
+                    'flex-1 flex items-center justify-center rounded-[18px] transition-all active:bg-slate-200/70 active:scale-95 z-2',
+                    type() === 'COMPLETE'
+                      ? CHALLENGE_TEXT_COLOR_500[props.color()]
+                      : 'text-gray-600'
+                  )}
+                >
+                  <input
+                    type='radio'
+                    name='challenge-item-type'
+                    id='complete'
+                    class='hidden'
+                    onChange={() => setType('COMPLETE')}
+                  />
+                  <CheckCheck
+                    size={28}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </label>
+                <label
+                  for='over'
+                  class={clsx(
+                    'flex-1 flex items-center justify-center rounded-[18px] transition-all active:bg-slate-200/70 active:scale-95 z-2',
+                    type() === 'OVER'
+                      ? CHALLENGE_TEXT_COLOR_500[props.color()]
+                      : 'text-gray-600'
+                  )}
+                >
+                  <input
+                    type='radio'
+                    name='challenge-item-type'
+                    id='over'
+                    class='hidden'
+                    onChange={() => setType('OVER')}
+                  />
+                  <ChevronsUp
+                    size={28}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </label>
+                <label
+                  for='under'
+                  class={clsx(
+                    'flex-1 flex items-center justify-center rounded-[18px] transition-all active:bg-slate-200/70 active:scale-95 z-2',
+                    type() === 'UNDER'
+                      ? CHALLENGE_TEXT_COLOR_500[props.color()]
+                      : 'text-gray-600'
+                  )}
+                >
+                  <input
+                    type='radio'
+                    name='challenge-item-type'
+                    id='under'
+                    class='hidden'
+                    onChange={() => setType('UNDER')}
+                  />
+                  <ChevronsDown
+                    size={28}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </label>
+                <div
+                  class='absolute top-[6px] bottom-[6px] bg-slate-200 transition-all duration-300 rounded-[18px]'
+                  style={{
+                    width: 'calc(calc(100% - 28px) / 3)',
+                    left: `calc(calc(calc(calc(100% - 28px) / 3) * ${typeIndicatorLevel()}) + ${
+                      typeIndicatorLevel() * 8 + 6
+                    }px)`,
+                  }}
+                ></div>
+              </div>
+            </Form.Wrapper>
+          </div>
 
-          {maxStep() !== null && (
-            <NewChallengeItemStepNavigator
-              maxStep={maxStep as Accessor<number>}
-              currentStep={currentStep}
-            />
-          )}
-
-          <NewChallengeItemStep.Type
-            displayType={getDisplayType('type')}
-            onNext={(type) => {
-              setType(type);
-              setStep('name');
-            }}
-          />
-
-          <NewChallengeItemStep.Name
-            name={name}
-            setName={setName}
-            displayType={getDisplayType('name')}
-            onPrev={() => setStep('type')}
-            onNext={() => {
-              if (type() === 'COMPLETE') {
-                setStep('day');
-              } else {
-                setStep('count');
-              }
-            }}
-          />
-
-          <NewChallengeItemStep.Count
-            count={count}
-            setCount={setCount}
-            unit={unit}
-            setUnit={setUnit}
-            displayType={getDisplayType('count')}
-            onPrev={() => setStep('name')}
-            onNext={() => setStep('day')}
-          />
-
-          <NewChallengeItemStep.Day
-            displayType={getDisplayType('day')}
-            day={day}
-            onChangeDay={handleChangeDay}
-            onPrev={() => {
-              if (type() === 'COMPLETE') {
-                setStep('name');
-              } else {
-                setStep('count');
-              }
-            }}
-            onNext={() => {
-              const _type = type();
-
-              if (_type === null) return;
-
-              toast.open(`🎉 '${name()}' is added!`);
-
-              const id = new Date().valueOf().toString();
-
-              if (_type === 'COMPLETE') {
-                props.onSubmit({
-                  id,
-                  type: 'COMPLETE',
-                  name: name(),
-                  days: day(),
-                  isNew: true,
-                });
-              } else {
-                props.onSubmit({
-                  id,
-                  type: _type,
-                  targetCount: Number(count()),
-                  unit: unit(),
-                  name: name(),
-                  days: day(),
-                  isNew: true,
-                });
-              }
-              close();
-            }}
-          />
-        </div>
+          <Panel.CTAButton color={props.color}>Add</Panel.CTAButton>
+        </>
       )}
-    </Panel.Blured>
+    </Panel.Slide>
   );
+};
+
+const Form = {
+  Wrapper: (props: { children: JSX.Element }) => {
+    return (
+      <div class='flex flex-col gap-2 mb-4 w-full'>
+        {children(() => props.children)()}
+      </div>
+    );
+  },
+  Label: (props: { children: JSX.Element }) => {
+    return (
+      <p class='font-semibold text-gray-700'>
+        {children(() => props.children)()}
+      </p>
+    );
+  },
 };
