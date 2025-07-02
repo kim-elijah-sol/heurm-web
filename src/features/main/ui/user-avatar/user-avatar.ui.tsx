b42e0f21791a542d3e6aa5ec5b8f97f17b48d33c
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-02 13:16:34 +0000

    fix: 유저 아바타 크기 찌그러지는 현상 방지

--- src/features/main/ui/user-avatar/user-avatar.ui.tsx
+++ src/features/main/ui/user-avatar/user-avatar.ui.tsx
@@ -15,7 +15,7 @@ export const UserAvatar: Component<Props> = (props) => {
       <div
         onClick={props.onClick}
         class={clsx(
-          'w-14 h-14 rounded-[42%] overflow-hidden border-gray-300 transition-all duration-300 active:scale-90',
+          'w-14 h-14 min-w-14 rounded-[42%] overflow-hidden border-gray-300 transition-all duration-300 active:scale-90',
           src() ? 'border' : ''
         )}
       >
