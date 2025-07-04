e680e3bbc6a139d07bc4d2c196e9597abdecca99
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-04 14:15:40 +0000

    feature: add formatter param at createInput

--- src/shared/hook/create-input.hook.ts
+++ src/shared/hook/create-input.hook.ts
@@ -1,10 +1,13 @@
 import { createSignal } from 'solid-js';
 
-export const createInput = (defaultValue: string = '') => {
+export const createInput = (
+  defaultValue: string = '',
+  formatter?: (value: string) => string
+) => {
   const [value, setValue] = createSignal<string>(defaultValue);
 
   const handleInputValue = (e: { target: HTMLInputElement }) =>
-    setValue(e.target.value);
+    setValue(formatter ? formatter(e.target.value) : e.target.value);
 
   return [value, handleInputValue] as const;
 };
