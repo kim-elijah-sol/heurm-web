22d4ad340c9b46d1482cc351f768f04368508e0e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-04 13:50:42 +0000

    rename: Challenge -> Flow at flowTitleSchema

--- src/shared/schema/flow-title.schema.ts
+++ src/shared/schema/flow-title.schema.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const flowTitleSchema = z
   .string()
   .min(2, {
-    message: 'Challenge title must be at least 2 characters long',
+    message: 'Flow title must be at least 2 characters long',
   })
   .max(16, {
-    message: 'Challenge title must be at most 16 characters long',
+    message: 'Flow title must be at most 16 characters long',
   });
