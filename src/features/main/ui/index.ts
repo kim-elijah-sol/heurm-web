62b123c92915d59965ecceddc667e6ff745d6dec
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-06 10:48:20 +0000

    remove: feature/main/no-challenge-item

--- src/features/main/ui/index.ts
+++ src/features/main/ui/index.ts
@@ -1,7 +1,6 @@
 export * from './challenge-item';
 export * from './date-card';
 export * from './hello-user';
-export * from './no-challenge-item';
 export * from './user-avatar';
 export * from './week-calendar';
 export * from './week-picker';
--- src/features/main/ui/no-challenge-item/index.ts
+++
@@ -1,6 +0,0 @@
-import { NoChallengeItem as _NoChallengeItem } from './no-challenge-item.ui';
-import { Today } from './today.ui';
-
-export const NoChallengeItem = Object.assign(_NoChallengeItem, {
-  Today,
-});
