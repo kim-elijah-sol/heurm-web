dd23e9644b8661a5631b56b55c4327f727e5e7cb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-06 10:30:28 +0000

    refactor: edit-challenge-item-panel -> edit-flow-panel 로 포팅

---
+++ src/panel-pages/edit-flow-panel.tsx
@@ -0,0 +1,420 @@
+import clsx from 'clsx';
+import { Match, Show, Switch, type Accessor, type Component } from 'solid-js';
+import { flowConstant, type FlowType } from '~/entities/flow';
+import { createEditFlowForm } from '~/features/flow/hook';
+import {
+  FlowPanelDatePicker,
+  FlowPanelForm,
+  FlowPanelMonthlyPatternSelect,
+  FlowPanelRadio,
+  FlowPanelWeeklyPatternSelect,
+  FlowPanelYearlyPatternSelect,
+} from '~/features/flow/ui';
+import { FLOW_BG_400, FLOW_BORDER_400, FLOW_TEXT_500 } from '~/shared/constant';
+import { toast } from '~/shared/lib';
+import {
+  Check,
+  CheckCheck,
+  ChevronsDown,
+  ChevronsUp,
+  MoveRight,
+  Panel,
+  X,
+} from '~/shared/ui';
+
+type Props = {
+  close: () => void;
+  flow: Accessor<FlowType.GetFlowResponseItem>;
+};
+
+export const EditFlowPanel: Component<Props> = (props) => {
+  const inputBaseClassName =
+    'font-semibold px-4 py-4 rounded-[24px] w-full transition-all bg-slate-100 focus:bg-slate-200 placeholder:text-gray-400';
+
+  const {
+    name,
+    handleInputName,
+    nameTitle,
+    color,
+    setColor,
+    type,
+    setType,
+    typeStep,
+    targetCount,
+    handleInputTargetCount,
+    unit,
+    handleInputUnit,
+    intervalType,
+    setIntervalType,
+    intervalTypeStep,
+    repeatType,
+    setRepeatType,
+    repeatTypeStep,
+    repeat,
+    setRepeat,
+    rest,
+    setRest,
+    weeklyPattern,
+    setWeeklyPattern,
+    days,
+    setDays,
+    monthlyPattern,
+    setMonthlyPattern,
+    dates,
+    setDates,
+    weeks,
+    setWeeks,
+    yearlyPattern,
+    setYearlyPattern,
+    months,
+    setMonths,
+    accumulate,
+    setAccumulate,
+    accumulateType,
+    setAccumulateType,
+    accumulateTypes,
+    accumulateTypeStep,
+    startAt,
+    setStartAt,
+    endAt,
+    setEndAt,
+    repeatUnit,
+    nRadioText,
+    getRepeatRadioText,
+    restPlaceholderText,
+    disabled,
+    handleSave,
+  } = createEditFlowForm(props.flow);
+
+  const yearlyPatternSelect = () => (
+    <FlowPanelYearlyPatternSelect
+      yearlyPattern={yearlyPattern}
+      setYearlyPattern={setYearlyPattern}
+      color={color}
+      months={months}
+      setMonths={setMonths}
+    />
+  );
+
+  const monthlyPatternSelect = () => (
+    <FlowPanelMonthlyPatternSelect
+      monthlyPattern={monthlyPattern}
+      setMonthlyPattern={setMonthlyPattern}
+      color={color}
+      dates={dates}
+      setDates={setDates}
+      weeks={weeks}
+      setWeeks={setWeeks}
+    />
+  );
+
+  const weeklyPatternSelect = () => (
+    <FlowPanelWeeklyPatternSelect
+      weeklyPattern={weeklyPattern}
+      setWeeklyPattern={setWeeklyPattern}
+      days={days}
+      setDays={setDays}
+      color={color}
+    />
+  );
+
+  const monthlyPatternSelects = () => (
+    <>
+      {monthlyPatternSelect()}
+
+      <Show when={monthlyPattern() !== 'Select Date'}>
+        {weeklyPatternSelect()}
+      </Show>
+    </>
+  );
+
+  return (
+    <Panel.Slide close={props.close} class='px-0'>
+      {(close) => (
+        <>
+          <div class='absolute flex items-center justify-between left-0 right-0 top-0 p-4 pb-2 bg-white/75 backdrop-blur-sm z-10'>
+            <p class='font-semibold text-2xl'>{nameTitle()}</p>
+            <button
+              onClick={close}
+              class='p-[7px] rounded-[42%] transition-all active:scale-[.95] bg-red-400 active:bg-red-500'
+            >
+              <X size={30} />
+            </button>
+          </div>
+          <div class='overflow-y-auto items-center pb-20 pt-[72px] px-4'>
+            <FlowPanelForm.Wrapper>
+              <FlowPanelForm.Label>Name</FlowPanelForm.Label>
+              <input
+                type='text'
+                class={inputBaseClassName}
+                value={name()}
+                onInput={handleInputName}
+                placeholder='Flow Name'
+              />
+            </FlowPanelForm.Wrapper>
+            <FlowPanelForm.Wrapper>
+              <FlowPanelForm.Label>Type</FlowPanelForm.Label>
+              <FlowPanelRadio step={typeStep}>
+                <FlowPanelRadio.Item
+                  color={color}
+                  checked={() => type() === 'COMPLETE'}
+                  onChange={() => setType('COMPLETE')}
+                  name='flow-type'
+                  id='complete'
+                >
+                  <CheckCheck
+                    size={24}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </FlowPanelRadio.Item>
+                <FlowPanelRadio.Item
+                  color={color}
+                  checked={() => type() === 'OVER'}
+                  onChange={() => setType('OVER')}
+                  name='flow-type'
+                  id='over'
+                >
+                  <ChevronsUp
+                    size={24}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </FlowPanelRadio.Item>
+                <FlowPanelRadio.Item
+                  color={color}
+                  checked={() => type() === 'UNDER'}
+                  onChange={() => setType('UNDER')}
+                  name='flow-type'
+                  id='under'
+                >
+                  <ChevronsDown
+                    size={24}
+                    stroke='currentColor'
+                    className='transition-all'
+                  />
+                </FlowPanelRadio.Item>
+              </FlowPanelRadio>
+            </FlowPanelForm.Wrapper>
+
+            <Show when={type() !== 'COMPLETE'}>
+              <FlowPanelForm.Wrapper>
+                <FlowPanelForm.Label>
+                  Target Count &nbsp;&&nbsp; Unit
+                </FlowPanelForm.Label>
+                <div class='flex w-full gap-2'>
+                  <input
+                    type='number'
+                    pattern='[0-9]*'
+                    inputMode='numeric'
+                    class={clsx(inputBaseClassName, 'flex-2')}
+                    placeholder='Target Count'
+                    value={targetCount()}
+                    onInput={handleInputTargetCount}
+                  />
+                  <input
+                    type='text'
+                    class={clsx(inputBaseClassName, 'flex-1')}
+                    placeholder='Unit'
+                    value={unit()}
+                    onInput={handleInputUnit}
+                  />
+                </div>
+
+                <label for='accumulate' class='mt-2'>
+                  <input
+                    type='checkbox'
+                    name='accumulate'
+                    id='accumulate'
+                    class='hidden'
+                    checked={accumulate()}
+                    onChange={(e) => setAccumulate(e.target.checked)}
+                  />
+                  <div class='flex items-center gap-2'>
+                    <div
+                      class={clsx(
+                        'p-[2px] rounded-[42%] border-2 transition-all',
+                        accumulate()
+                          ? clsx(
+                              FLOW_BG_400[color()],
+                              FLOW_BORDER_400[color()],
+                              'text-white'
+                            )
+                          : 'border-gray-200 text-gray-300'
+                      )}
+                    >
+                      <Check size={16} strokeWidth={3} stroke='currentColor' />
+                    </div>
+
+                    <p
+                      class={clsx(
+                        'font-semibold transition-all',
+                        accumulate() ? 'text-gray-600' : 'text-gray-400'
+                      )}
+                    >
+                      Accumulate
+                    </p>
+                  </div>
+                </label>
+
+                <Show when={accumulate()}>
+                  <FlowPanelRadio step={accumulateTypeStep}>
+                    {accumulateTypes().map((it) => (
+                      <FlowPanelRadio.Item
+                        color={color}
+                        checked={() => accumulateType() === it}
+                        onChange={() => setAccumulateType(it)}
+                        name='flow-accumulate-type'
+                        id={it.toLowerCase()}
+                      >
+                        <p class='font-semibold text-sm'>{it}</p>
+                      </FlowPanelRadio.Item>
+                    ))}
+                  </FlowPanelRadio>
+                </Show>
+              </FlowPanelForm.Wrapper>
+            </Show>
+
+            <FlowPanelForm.Divider />
+
+            <FlowPanelForm.Wrapper>
+              <FlowPanelForm.Label>Interval Type</FlowPanelForm.Label>
+              <FlowPanelRadio step={intervalTypeStep}>
+                {flowConstant.INTERVAL_TYPES.map((it) => (
+                  <FlowPanelRadio.Item
+                    color={color}
+                    checked={() => intervalType() === it}
+                    onChange={() => setIntervalType(it)}
+                    name='flow-interval-type'
+                    id={it.toLowerCase()}
+                  >
+                    <p class='font-semibold text-sm'>{it}</p>
+                  </FlowPanelRadio.Item>
+                ))}
+              </FlowPanelRadio>
+            </FlowPanelForm.Wrapper>
+
+            <Show when={intervalType() !== 'DAILY'}>
+              <FlowPanelForm.Wrapper>
+                <FlowPanelForm.Label>Interval Pattern</FlowPanelForm.Label>
+
+                <Switch>
+                  <Match when={intervalType() === 'WEEKLY'}>
+                    {weeklyPatternSelect()}
+                  </Match>
+
+                  <Match when={intervalType() === 'MONTHLY'}>
+                    {monthlyPatternSelects()}
+                  </Match>
+
+                  <Match when={intervalType() === 'YEARLY'}>
+                    {yearlyPatternSelect()}
+
+                    {monthlyPatternSelects()}
+                  </Match>
+                </Switch>
+              </FlowPanelForm.Wrapper>
+
+              <FlowPanelForm.Divider />
+            </Show>
+
+            <FlowPanelForm.Wrapper>
+              <FlowPanelForm.Label>Repeat Type</FlowPanelForm.Label>
+              <FlowPanelRadio step={repeatTypeStep}>
+                {flowConstant.REPEAT_TYPES.map((it) => (
+                  <FlowPanelRadio.Item
+                    color={color}
+                    checked={() => repeatType() === it}
+                    onChange={() => setRepeatType(it)}
+                    name='flow-repeat-type'
+                    id={it.toLowerCase()}
+                  >
+                    <p class='font-semibold text-sm'>
+                      {getRepeatRadioText(it)}
+                    </p>
+                  </FlowPanelRadio.Item>
+                ))}
+              </FlowPanelRadio>
+
+              {repeatType() !== 'EVERY' && (
+                <div class='flex w-full gap-2 items-center justify-between'>
+                  <p
+                    class={clsx(
+                      'text-sm font-semibold whitespace-nowrap pl-4 flex-1',
+                      FLOW_TEXT_500[color()]
+                    )}
+                  >
+                    {repeatType() === 'N' &&
+                      nRadioText().replace('N', repeat() || 'N')}
+
+                    {repeatType() === 'NM' && (
+                      <>
+                        {`${repeat() || 'N'} ${repeatUnit()} on,`}
+                        <br />
+                        {`${rest() || 'M'} ${repeatUnit()} off`}
+                      </>
+                    )}
+                  </p>
+
+                  <div class='flex gap-2 flex-2'>
+                    <input
+                      type='number'
+                      pattern='[0-9]*'
+                      inputMode='numeric'
+                      class={inputBaseClassName}
+                      placeholder={nRadioText().replace('Every', '')}
+                      value={repeat()}
+                      onInput={(e) => setRepeat(e.target.value)}
+                    />
+                    {repeatType() === 'NM' && (
+                      <input
+                        type='number'
+                        pattern='[0-9]*'
+                        inputMode='numeric'
+                        class={inputBaseClassName}
+                        placeholder={restPlaceholderText()}
+                        value={rest()}
+                        onInput={(e) => setRest(e.target.value)}
+                      />
+                    )}
+                  </div>
+                </div>
+              )}
+            </FlowPanelForm.Wrapper>
+
+            <FlowPanelForm.Divider />
+
+            <FlowPanelForm.Wrapper>
+              <FlowPanelForm.Label>Period</FlowPanelForm.Label>
+
+              <div class='flex items-center justify-between gap-4 relative'>
+                <FlowPanelDatePicker date={startAt} onChange={setStartAt} />
+                <MoveRight />
+                <FlowPanelDatePicker
+                  date={endAt}
+                  onChange={setEndAt}
+                  placeholder='End Date'
+                  removable
+                />
+              </div>
+            </FlowPanelForm.Wrapper>
+          </div>
+
+          <Panel.CTAButton
+            color={color}
+            disabled={disabled()}
+            onClick={async () => {
+              await handleSave();
+
+              close();
+
+              toast.open(`🎉 '${name()}' Flow is edited!`);
+            }}
+          >
+            Save
+          </Panel.CTAButton>
+        </>
+      )}
+    </Panel.Slide>
+  );
+};
