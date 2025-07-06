23dd932683f9f0ccd140619f9937e649ddef6cc1
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-06 10:44:28 +0000

    remove: entities/new-challenge

--- src/entities/new-challenge/index.ts
+++
@@ -1,4 +0,0 @@
-export * as newChallengeApi from './new-challenge.api';
-export * as newChallengeQueries from './new-challenge.queries';
-export * as newChallengeSchema from './new-challenge.schema';
-export * as NewChallengeType from './new-challenge.type';
