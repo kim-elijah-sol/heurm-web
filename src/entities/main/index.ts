8394392455a2d8a3478750f822e261575fa69a09
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-23 13:01:52 +0000

    remove: challenge 도메인을 담고 있는 /entities/main

--- src/entities/main/index.ts
+++ src/entities/main/index.ts
@@ -1,5 +1,1 @@
-export * as mainApi from './main.api';
 export * as mainConstant from './main.constant';
-export * as mainQueries from './main.queries';
-export * as mainSchema from './main.schema';
-export * as MainType from './main.type';
