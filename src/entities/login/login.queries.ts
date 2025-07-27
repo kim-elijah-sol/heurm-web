775da9cd77cd2c34b9190be29e8d51e8d4fd176a
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-27 10:35:19 +0000

    feature: login query key 등록

--- src/entities/login/login.queries.ts
+++ src/entities/login/login.queries.ts
@@ -1,13 +1,18 @@
+import { createQueryKeys } from '@lukemorales/query-key-factory';
 import { useMutation } from '@tanstack/solid-query';
 import { toastAtError } from '~/shared/fx';
 import { postLogin } from './login.api';
+
+export const keys = createQueryKeys('login', {
+  post: ['post'],
+});
 
 export const postLoginMutation = (
   onSuccess: (data: Awaited<ReturnType<typeof postLogin>>) => void
 ) =>
   useMutation(() => ({
+    mutationKey: keys.post.queryKey,
     mutationFn: postLogin,
-    mutationKey: ['postLogin'],
     onSuccess,
     onError: (error) => toastAtError(error),
   }));
