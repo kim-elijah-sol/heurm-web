27e49cd5915eaa0c534f285655ab765efae76bbf
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-27 13:10:42 +0000

    feature: login form 내 input 스타일 변경

--- src/widgets/login/login-form.ui.tsx
+++ src/widgets/login/login-form.ui.tsx
@@ -15,7 +15,7 @@ export const LoginForm = () => {
   const buttonBaseClassName = 'p-5 rounded-[42%] transition-all';
 
   const inputBaseClassName =
-    'font-semibold px-4 py-3 rounded-[12px] w-full transition-all bg-slate-100 focus:bg-slate-200';
+    'font-semibold px-4 py-4 rounded-[24px] w-full transition-all bg-slate-100 focus:bg-slate-200 placeholder:text-gray-400';
 
   return (
     <form
