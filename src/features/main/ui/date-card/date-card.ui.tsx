baaecaa3085a69bddfd25c1880e23da18084e576
kim-elijah-sol <kimelijahsol@gmail.com>
2025-07-29 14:22:32 +0000

    remove: date-select, date-card 내 불필요한 props 제거

--- src/features/main/ui/date-card/date-card.ui.tsx
+++ src/features/main/ui/date-card/date-card.ui.tsx
@@ -7,16 +7,9 @@ type Props = {
   isCurrent: Accessor<boolean>;
   isToday: Accessor<boolean>;
   onClick: JSX.CustomEventHandlersCamelCase<HTMLDivElement>['onClick'];
-  all: Accessor<number>;
-  win: Accessor<number>;
-  lose: Accessor<number>;
 };
 
 export const DateCard: Component<Props> = (props) => {
-  const winHeight = () => (props.win() / props.all()) * 100;
-
-  const loseHeight = () => (props.lose() / props.all()) * 100;
-
   return (
     <div
       class={clsx(
@@ -33,31 +26,6 @@ export const DateCard: Component<Props> = (props) => {
 
       {props.isToday() && (
         <div class='absolute left-1/2 bottom-[6px] -translate-x-1/2 w-1 h-1 rounded-full bg-white z-2' />
-      )}
-
-      {props.all() > 0 && (
-        <>
-          <div
-            style={{
-              height: `${winHeight()}%`,
-            }}
-            class={clsx(
-              'absolute left-0 right-0 bottom-0 z-0 transition-all',
-              props.isCurrent() ? 'bg-emerald-400' : 'bg-emerald-300/90'
-            )}
-          />
-
-          <div
-            style={{
-              bottom: `${winHeight()}%`,
-              height: `${loseHeight()}%`,
-            }}
-            class={clsx(
-              'absolute left-0 right-0 z-0 transition-all',
-              props.isCurrent() ? 'bg-rose-400' : 'bg-rose-300/90'
-            )}
-          />
-        </>
       )}
     </div>
   );
--- src/widgets/main/date-select.ui.tsx
+++ src/widgets/main/date-select.ui.tsx
@@ -1,4 +1,3 @@
-import { format } from 'date-fns';
 import { Index } from 'solid-js';
 import { getMidnight, isSameDate } from '~/features/main/fx';
 import { createDateSelect } from '~/features/main/hook';
@@ -6,8 +5,6 @@ import { DateCard, WeekPicker } from '~/features/main/ui';
 
 export const DateSelect = () => {
   const { current, weeks, setCurrent } = createDateSelect();
-
-  const data = () => [] as any;
 
   return (
     <div class='mb-4'>
@@ -17,17 +14,12 @@ export const DateSelect = () => {
       <div class='flex gap-2 justify-between'>
         <Index each={weeks()}>
           {(date) => {
-            const formatedDate = () => format(date(), 'yyyy-MM-dd');
-
             return (
               <DateCard
                 date={date}
                 isCurrent={() => isSameDate(date(), current())}
                 isToday={() => isSameDate(date(), getMidnight())}
                 onClick={() => setCurrent(date())}
-                all={() => data()?.[formatedDate()]?.all ?? 0}
-                win={() => data()?.[formatedDate()]?.win ?? 0}
-                lose={() => data()?.[formatedDate()]?.lose ?? 0}
               />
             );
           }}
