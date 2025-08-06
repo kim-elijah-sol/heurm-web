bbbea6dc42eeaa3d8c5bf74996781932bb248ddb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-06 12:52:06 +0000

    feature: navigation 로그인 페이지에서 미노출

--- src/features/layout/ui/navigation/navigation.ui.tsx
+++ src/features/layout/ui/navigation/navigation.ui.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from '@solidjs/router';
 import { For, type Component } from 'solid-js';
 import { LayoutConstant } from '~/entities/layout';
 import { NavigationAnchor } from './navigation-anchor.ui';
@@ -5,6 +6,12 @@ import { NavigationCurrentIndicator } from './navigation-current-indicator.ui';
 import './navigation.ui.css';
 
 export const Navigation: Component<{}> = () => {
+  const location = useLocation();
+
+  const pathname = () => location.pathname;
+
+  if (pathname() === '/login') return <></>;
+
   return (
     <nav class='heurm-navigation fixed bottom-4 left-1/2 -translate-x-1/2 p-1 bg-white z-30 rounded-[32px] flex justify-between w-max'>
       <For each={LayoutConstant.NAVIGATION_ANCHORS}>
