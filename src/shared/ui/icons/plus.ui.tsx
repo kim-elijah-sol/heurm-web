2fda8383af96f6e3f0c1eba14c12edff61024bae
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-10 11:04:26 +0000

    feature: plus ui 컴포넌트에 strokeWidth prop 추가

--- src/shared/ui/icons/plus.ui.tsx
+++ src/shared/ui/icons/plus.ui.tsx
@@ -3,10 +3,12 @@ import clsx from 'clsx';
 export const Plus = ({
   size = 24,
   stroke = '#FFFFFF',
+  strokwWidth = 2,
   className,
 }: {
   size?: number;
   stroke?: string;
+  strokwWidth?: number;
   className?: string;
 }) => (
   <svg
@@ -16,7 +18,7 @@ export const Plus = ({
     viewBox='0 0 24 24'
     fill='none'
     stroke={stroke}
-    stroke-width='2'
+    stroke-width={strokwWidth}
     stroke-linecap='round'
     stroke-linejoin='round'
     class={clsx('lucide lucide-plus-icon lucide-plus', className)}
