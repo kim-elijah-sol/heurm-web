c74ef4ead71072d5f9c74daf422545dc101821a4
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-10 13:20:16 +0000

    refactor: hello-user, user-avatar slice 이동 main -> user

--- src/features/main/ui/index.ts
+++ src/features/main/ui/index.ts
@@ -1,5 +1,3 @@
 export * from './date-card';
-export * from './hello-user';
-export * from './user-avatar';
 export * from './week-calendar';
 export * from './week-picker';
--- src/features/user/ui/index.ts
+++ src/features/user/ui/index.ts
@@ -1,3 +1,5 @@
+export * from './hello-user';
+export * from './user-avatar';
 export * from './user-setting-cancel-account-button';
 export * from './user-setting-form';
 export * from './user-setting-profile';
