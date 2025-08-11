054c355727d140fed9fd139f66718fb316bf00a6
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-11 10:13:28 +0000

    feature: analytics-item 시작점 빈요일만큼 채우기

--- src/features/analytics/ui/analytics-item/analytics-item.ui.tsx
+++ src/features/analytics/ui/analytics-item/analytics-item.ui.tsx
@@ -19,8 +19,16 @@ export const AnalyticsItem: Component<Props> = (props) => {
   const result = () =>
     props.analyticsCalcFx(props.startDate)(flow())(history.data ?? []);
 
+  const day = () => props.startDate.getDay();
+
   return (
     <div class='flex flex-col flex-wrap gap-[2px] h-24 items-start w-max'>
+      <div
+        class='w-3'
+        style={{
+          height: `${day() * 12 + Math.max(day() - 1, 0) * 2}px`,
+        }}
+      ></div>
       <For each={result()}>
         {(it) => <div class='w-3 h-3 bg-black rounded-[4px]'></div>}
       </For>
