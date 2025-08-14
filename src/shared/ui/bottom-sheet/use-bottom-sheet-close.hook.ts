1448986a8a328d85f18f42db744bb35010b637c2
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-14 13:47:26 +0000

    feature: BottomSheet 닫기 함수 hook 추가

---
+++ src/shared/ui/bottom-sheet/use-bottom-sheet-close.hook.ts
@@ -0,0 +1,11 @@
+import { useContext } from 'solid-js';
+import { BottomSheetCloseContext } from './bottom-sheet-close.context';
+
+export const useBottomSheetClose = () => {
+  const bottomSheetCloseContext = useContext(BottomSheetCloseContext);
+
+  if (!bottomSheetCloseContext)
+    throw new Error('Flow Item Color Context is not exist');
+
+  return bottomSheetCloseContext;
+};
