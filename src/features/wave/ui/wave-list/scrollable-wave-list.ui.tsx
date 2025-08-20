501761ccbaf1ab31a22458a6565a0c6bdc127b68
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-20 13:56:54 +0000

    feature: scrollable-wave-list 컴포넌트에 className prop 추가

--- src/features/wave/ui/wave-list/scrollable-wave-list.ui.tsx
+++ src/features/wave/ui/wave-list/scrollable-wave-list.ui.tsx
@@ -1,12 +1,14 @@
+import clsx from 'clsx';
 import { type Component, type JSX } from 'solid-js';
 
 type Props = {
   children: JSX.Element;
+  className?: string;
 };
 
 export const ScrollableWaveList: Component<Props> = (props) => {
   return (
-    <div class='overflow-x-auto'>
+    <div class={clsx('overflow-x-auto', props.className)}>
       <div class='w-max flex gap-2'>{props.children}</div>
     </div>
   );
