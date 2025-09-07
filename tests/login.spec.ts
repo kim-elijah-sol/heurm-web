af4f3c9176c0ff8f6c286febf0363a7c846522fd
kim-elijah-sol <kimelijahsol@gmail.com>
2025-09-07 13:13:54 +0000

    test: 로그인 페이지 테스트 > 비밀번호 형식이 아닌 경우 로그인 시도 시 실패 토스트가 나온다. [영문 미포함]

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -84,4 +84,19 @@ test.describe('로그인 페이지 테스트', () => {
       'Password must be at most 16 characters long'
     );
   });
+
+  test('비밀번호 형식이 아닌 경우 로그인 시도 시 실패 토스트가 나온다. [영문 미포함]', async ({
+    page,
+  }) => {
+    await page.goto('/login');
+
+    await page.locator('input[type="email"]').fill('kimelijahsol@test.com');
+    await page.locator('input[type="password"]').fill('132465798~!');
+
+    await page.locator('button[type="submit"]').click();
+
+    await expect(page.locator('.toast')).toHaveText(
+      'Password must contain at least one letter'
+    );
+  });
 });
