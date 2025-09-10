73b5125d73599b3dd8bb1b7862aff47729fcd4b8
kim-elijah-sol <kimelijahsol@gmail.com>
2025-09-10 14:34:19 +0000

    test: [ct] shared/ui/flow-color-select

---
+++ tests/ct/shared/ui/flow-color-select/flow-color-select.ui.spec.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@solidjs/testing-library';
+import userEvent from '@testing-library/user-event';
+import { expect } from 'playwright/test';
+import { createSignal } from 'solid-js';
+import { describe, test } from 'vitest';
+import { FlowColor } from '~/shared/types';
+import { FlowColorSelect } from '~/shared/ui';
+
+describe('flow-color-select', () => {
+  test('초기 색상이 선택되어 있다.', async () => {
+    const component = render(() => (
+      <FlowColorSelect color={() => 'blue'} setColor={() => {}} />
+    ));
+
+    const blueButton = component.getByTestId('color-item-blue');
+
+    expect(blueButton.children.length).toBe(1);
+
+    const redButton = component.getByTestId('color-item-red');
+
+    expect(redButton.children.length).toBe(0);
+  });
+
+  test('사용자 클릭을 정상적으로 처리할 수 있다.', async () => {
+    const [color, setColor] = createSignal<FlowColor>('blue');
+
+    const component = render(() => (
+      <FlowColorSelect color={color} setColor={setColor} />
+    ));
+
+    const redButton = component.getByTestId('color-item-red');
+
+    await userEvent.click(redButton);
+
+    expect(redButton.children.length).toBe(1);
+
+    expect(color()).toBe('red');
+  });
+
+  test('사용자 지정 className 을 컨테이너에 정상적으로 적용할 수 있다.', () => {
+    render(() => (
+      <FlowColorSelect
+        color={() => 'blue'}
+        setColor={() => {}}
+        className='test-className'
+      />
+    ));
+
+    const component = screen.getByTestId('flow-color-select');
+
+    expect(component.className).toContain('test-className');
+  });
+});
