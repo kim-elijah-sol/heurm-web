0bdbbe001f8351c51403e45686a35f9f1ac6c994
kim-elijah-sol <kimelijahsol@gmail.com>
2025-09-14 14:14:00 +0000

    test: [unit] features/flow/fx/find-current-histrory > toBe 추가

--- tests/unit/features/flow/fx/find-current-history.fx.spec.ts
+++ tests/unit/features/flow/fx/find-current-history.fx.spec.ts
@@ -7,7 +7,7 @@ describe('find-current-history', () => {
       findCurrentHistory(() => new Date('2025-09-14 09:00:00'))({
         date: '2025-09-14 09:00:00',
       })
-    );
+    ).toBe(true);
   });
 
   test('현재 조회 중인 날짜에 적합한 history 라면 true 를 반환한다. [일자만 일치]', () => {
@@ -15,7 +15,7 @@ describe('find-current-history', () => {
       findCurrentHistory(() => new Date('2025-09-14 21:00:00'))({
         date: '2025-09-14 09:00:00',
       })
-    );
+    ).toBe(true);
   });
 
   test('현재 조회 중인 날짜에 적합하지 않은 history 라면 false 를 반환한다.', () => {
@@ -23,6 +23,6 @@ describe('find-current-history', () => {
       findCurrentHistory(() => new Date('2025-09-13 09:00:00'))({
         date: '2025-09-14 09:00:00',
       })
-    );
+    ).toBe(false);
   });
 });
